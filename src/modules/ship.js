df375e0968467ff33580cfbc81620d401d174476
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-03 19:30:21 -0600

    created outline of ship module

---
+++ src/modules/ship.js
@@ -0,0 +1,26 @@
+const ship = {
+
+	
+	//create a shipLength() helper function that outputs ship's length
+
+	//create hit() helper function that takes a 
+	// number and then marks that position as ‘hit’
+
+	//create isSunk() helper function should be a function that calculates it 
+	// based on their length and whether all of their positions are ‘hit’
+
+
+
+	// Your 'ships' method will be objects that include their length,
+	// where they’ve been hit and whether or not they’ve been sunk.
+
+	// REMEMBER you only have to test your object’s public interface. 
+	// Only methods or properties that are used outside of your ‘ship’ 
+	// object need unit tests.
+	ships() {
+
+	}
+
+};
+
+module.exports = ship;
