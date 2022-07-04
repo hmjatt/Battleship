381dd862eeb2f8d4cee10e441b4f4744a0e1d4d3
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-03 20:14:03 -0600

    adding comments to ship module

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,7 +1,12 @@
 const ship = {
 
 	
+	//create shipPosition() helper function that takes
+	// it should take two things [start, end] output and where on the board it is
+
 	//create a shipLength() helper function that outputs ship's length
+	// it should take output from shipPosition() function and should output how long 
+	// is the ship 
 
 	//create hit() helper function that takes a 
 	// number and then marks that position as ‘hit’
@@ -11,12 +16,14 @@ const ship = {
 
 
 
-	// Your 'ships' method will be objects that include their length,
-	// where they’ve been hit and whether or not they’ve been sunk.
+
 
 	// REMEMBER you only have to test your object’s public interface. 
 	// Only methods or properties that are used outside of your ‘ship’ 
 	// object need unit tests.
+
+	// Your 'ships' method will be objects that include their length,
+	// where they’ve been hit and whether or not they’ve been sunk.
 	ships() {
 
 	}
