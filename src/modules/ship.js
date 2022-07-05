5572aa8e05f5ed7c491fc96756e06d14f5259497
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-04 23:18:39 -0600

    added shipArray

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,5 +1,9 @@
 const ship = {
 
+	// width variable representing grid with and numbers of elements in gameGrid()
+	// function ships need to spaced out
+	width : 10,
+
 	/**
 	* A Ship is an array with all (x, y) coordinates of the ship:
 	*
@@ -10,6 +14,9 @@ const ship = {
 	*	{'x':4, 'y':2}
 	* ]
 	*/
+
+
+	//////////////// test code from here /////////////
 
 	
 
@@ -27,11 +34,15 @@ const ship = {
 	*	80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 
 	*	90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 	*/
+
+
+
+
 	gameGrid() {
 
 		let gridArray = [];
 
-		for (let i = 0; i < 10 * 10; i++) {
+		for (let i = 0; i < this.width * this.width; i++) {
 			gridArray.push(i);
 		}
 
@@ -39,22 +50,61 @@ const ship = {
 
 	},
 
-	// create a random ship size variable which includes 
-	//array of objects with range of values [{2,2}, {3,2}, {4,2}]
-	//
-	// shipSize : [{2,2}, {3,2}, {4,2}],
+	/* create shipArray, which is  that generates a grid
+	*  it is a array of 10 x 10 elements(100) filled with 0's
+	*  e.g.
+	*	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+	*	10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
+	*	20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+	*	30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+	*	40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+	*	50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 
+	*	60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 
+	*	70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
+	*	80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 
+	*	90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+	*/
 
-	//////////////// test code from here /////////////
 
-	//create placeShip() helper function that takes grid, size of ship
-	// places a ship at a random position on grid, outputs ships co-ordinates
-	// co-ordinates start at 00 end at 99
-	placeShip(gridArray, shipSize) {
-
-	}
+	shipArray : [
+		{
+			name: 'destroyer',
+			directions: [
+				[0, 1],
+				[0, this.width]
+			]
+		},
+		{
+			name: 'submarine',
+			directions: [
+				[0, 1, 2],
+				[0, this.width, this.width * 2]
+			]
+		},
+		{
+			name: 'cruiser',
+			directions: [
+				[0, 1, 2],
+				[0, this.width, this.width * 2]
+			]
+		},
+		{
+			name: 'battleship',
+			directions: [
+				[0, 1, 2, 3],
+				[0, this.width, this.width * 2, this.width * 3]
+			]
+		},
+		{
+			name: 'carrier',
+			directions: [
+				[0, 1, 2, 3, 4],
+				[0, this.width, this.width * 2, this.width * 3, this.width * 4]
+			]
+		},
+	],
 
 	
-
 
 	//////////// test code till here /////////////
 
