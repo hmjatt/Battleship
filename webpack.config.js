d8ccfe5e53f3a73ed9987ee3f929d410ba753bb0
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-06 01:07:32 -0600

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    entry: './src/public/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+		clean: true,
+		assetModuleFilename: '[name][ext]',
+    },
+	devtool: 'source-map',
+    module: {
+        rules: 
+        [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					}
+				}
+			},
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Weather App',
+			filename: 'index.html',
+			template: './src/public/template.html',
+			favicon: './src/public/images/cloudy.svg',
+		}),
+		new Dotenv(),
+	]
 };
